d313eddc4f101f46eefda08035ab7e344d39d8dd
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-11-07 16:34:28 -0500

    "Created class 'Reservation' and introduced 'if' statement specifying maximum amount of guests."

--- Restaurant/src/main/java/com/pluralsight/Reservation.java
+++ Restaurant/src/main/java/com/pluralsight/Reservation.java
@@ -1,20 +1,20 @@
 package com.pluralsight;
 
-public class Reservation {
-    //defining a class for handling reservations + declaring variables
-    int guestCount;
-    int restaurantCapacity;
-    boolean isRestaurantOpen;
-    boolean isConfirmed;
+    public class Reservation {
+        //defining a class for handling reservations + declaring variables
+        int guestCount;
+        int restaurantCapacity;
+        boolean isRestaurantOpen;
+        boolean isConfirmed;
 
-    public Reservation(int count, int capacity, boolean open) {
-        if (count < 1 || count > 8) {
-            System.out.println("Invalid reservation!");
+        public Reservation(int count, int capacity, boolean open) {
+            if (count < 1 || count > 8) {
+                System.out.println("Invalid reservation!");
+            }
+            guestCount = count;
+            restaurantCapacity = capacity;
+            isRestaurantOpen = open;
+
         }
-        guestCount = count;
-        restaurantCapacity = capacity;
-        isRestaurantOpen = open;
-
     }
-}
 
